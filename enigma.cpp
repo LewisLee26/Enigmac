1343c150186c44036521877255558d84505ffb00
Lewis Lee <lewis.patrick.lee@gmail.com>
2023-07-01 21:54:25 +0100

    Written enigma.cpp

---
+++ enigma.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <algorithm>
+
+using namespace std;
+
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+class Connector {
+    private:
+        string characterPairs;
+    public:
+        Connector(string characterPairsInput){
+            characterPairs = characterPairsInput;
+        }
+        void setCharacterPairs(char characterPairsInput[26]){
+            characterPairs = characterPairsInput;
+        }
+        string getCharacterPairs(){
+            return characterPairs;
+        }
+        void resetCharacterPairs(){
+            characterPairs = alphabet;
+        }
+        char getCharacterPair(char characterInput){
+            int index = alphabet.find(characterInput);
+            return characterPairs.at(index % 26);
+        }
+};
+
+class Rotor: public Connector {
+    private:
+        int rotorPosition;
+    public:
+        Rotor(string characterPairsInput, int rotorPositionInput) : Connector(characterPairsInput) {
+            rotorPosition = rotorPositionInput;
+        }
+        char getCharacterPair(char characterInput, bool forward){
+            int index;
+            if (forward){
+                index = alphabet.find(characterInput);
+                return getCharacterPairs().at((26+index - rotorPosition) % 26);
+            }else{
+                index = getCharacterPairs().find(characterInput);
+                return alphabet.at((index + rotorPosition) % 26);
+            }
+        }
+        int getRotorPosition(){
+            return rotorPosition;
+        }
+        void setRotorPosition(int positionInput){
+            rotorPosition = positionInput % 26;
+        }
+        void incrementRotorPosition(){
+            rotorPosition++;
+
+        }
+
+};
+
+class Plugboard: public Connector {
+    public:
+        Plugboard(string characterPairsInput) : Connector(characterPairsInput) {}
+        void swapCharacters(string characterPair){
+            char tempChar = getCharacterPairs().at(alphabet.find(characterPair.at(0)));
+            getCharacterPairs().at(alphabet.find(characterPair.at(0))) = getCharacterPairs().at(alphabet.find(characterPair.at(1)));
+            getCharacterPairs().at(alphabet.find(characterPair.at(1))) = tempChar;
+        }
+        void setCharacterPairs(string characterPairs) {
+            for (int i = 0; i < 13; i++) {
+                swapCharacters({characterPairs[i * 2], characterPairs[i * 2 + 1]});
+            }
+        }
+};
+
+string rotors[8] = {
+        "ekmflgdqvzntowyhxuspaibrcj",
+        "ajdksiruxblhwtmcqgznpyfvoe",
+        "bdfhjlcprtxvznyeiwgakmusqo",
+        "esovpzjayquirhxlnftgkdcmwb",
+        "vzbrgityupsdnhlxawmjqofeck",
+        "jpgvoumfyqbenhzrdkasxlictw",
+        "nzjhgrcxmyswboufaivlpekqdt",
+        "fkqhtlxocbjspdzramewniuygv"
+};
+
+string reflectors[3] = {
+        "ejmzalyxvbwfcrquontspikhgd",
+        "nzjhgrcxmyswboufaivlpekqdt",
+        "fkqhtlxocbjspdzramewniuygv"
+};
+
+char rotorPass(Rotor rotorSlots[3], char currentChar, bool forward){
+    for (int j = 0; j < 3; j++){
+        currentChar = rotorSlots[j].getCharacterPair(currentChar, forward);
+    }
+    return currentChar;
+}
+
+string enigma(string textInput, Rotor rotorSlot1, Rotor rotorSlot2, Rotor rotorSlot3, Plugboard plugboard, Connector reflector){
+    // standardize the text, change umlauts to ae, ue, oe ect
+    char currentChar;
+    string outputString;
+    Rotor rotorSlots[3] = {rotorSlot3, rotorSlot2, rotorSlot1};
+    for (int i = 0; i < textInput.length(); i++){
+        rotorSlots[0] = rotorSlot3;
+        rotorSlots[2] = rotorSlot1;
+        currentChar = plugboard.getCharacterPair(textInput.at(i));
+        currentChar = rotorPass(rotorSlots, currentChar, true);
+        currentChar = reflector.getCharacterPair(currentChar);
+        rotorSlots[0] = rotorSlot1;
+        rotorSlots[2] = rotorSlot3;
+        currentChar = rotorPass(rotorSlots, currentChar, false);
+        currentChar = plugboard.getCharacterPair(currentChar);
+
+        // not getting appended correctly
+        outputString.append(1,currentChar);
+        rotorSlot1.incrementRotorPosition();
+        if (rotorSlot1.getRotorPosition() == 26){
+            rotorSlot1.setRotorPosition(0);
+            rotorSlot2.incrementRotorPosition();
+            if (rotorSlot2.getRotorPosition() == 26){
+                rotorSlot2.setRotorPosition(0);
+                rotorSlot3.incrementRotorPosition();
+                if (rotorSlot3.getRotorPosition() == 26){
+                    rotorSlot3.setRotorPosition(0);
+                }
+            }
+        }
+    }
+
+    return outputString;
+}
+
+int main() {
+    Rotor rotor1(rotors[0], 0);
+    Rotor rotor2( rotors[1], 0);
+    Rotor rotor3(rotors[2], 0);
+    Plugboard plugboard(alphabet);
+
+    Connector reflector(reflectors[0]);
+    // Connector reflector(alphabet);
+    string textInput = "helloworld";
+    rotor1.setRotorPosition(0);
+    rotor2.setRotorPosition(0);
+    rotor3.setRotorPosition(0);
+    string encrypted = enigma(textInput, rotor1, rotor2, rotor3, plugboard, reflector);
+    cout << encrypted << endl;
+    rotor1.setRotorPosition(0);
+    rotor2.setRotorPosition(0);
+    rotor3.setRotorPosition(0);
+    string decrypted = enigma(encrypted, rotor1, rotor2, rotor3, plugboard, reflector);
+    cout << decrypted << endl;
+
+    return 0;
+}
